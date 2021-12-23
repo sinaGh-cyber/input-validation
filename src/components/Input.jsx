e9ae0f4293908a917d1ba312058aa9d6a08759ea
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-12-24 01:08:42 +0330

    validitor ready
    but quera dont accept it

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
-import { validate } from '../utils/validators'
-
-const INPUT_STATES = {
-  UNTOUCHED: 'UNTOUCHED',
-  VALID: 'VALID',
-  INVALID: 'INVALID',
-}
+import React from 'react';
+import { useState } from 'react';
+import { validate } from '../utils/validators';
 
 const Input = ({ label, id, type, validators, errorText }) => {
-  return (
-    <div className="form-input" data-testid="form-input">
-      <form >
-        <label onBlur={(e) => {
-          
-        }} htmlFor='id'>{label}</label>
-        <input type={type} id={id} />
-        </form>
-      
-      <p></p>
-    </div>
-  )
-}
+  const [INPUT_STATES, INPUT_STATES_SET] = useState({
+    UNTOUCHED: true,
+    isValid: true,
+  });
 
-export default Input
+  return (
+    <div
+      className={`form-input ${
+        INPUT_STATES.isValid ? '' : 'form-input--invalid'
+      } `}
+      data-testid="form-input"
+    >
+      <label htmlFor="id">{label}</label>
+      <input
+        onChange={(e) => {
+          if (INPUT_STATES.UNTOUCHED) return;
+          INPUT_STATES.isValid = validate(e.target.value, validators);
+          INPUT_STATES_SET({ ...INPUT_STATES });
+        }}
+        onBlur={(e) => {
+          if (INPUT_STATES.UNTOUCHED) {
+            INPUT_STATES.UNTOUCHED = false;
+            INPUT_STATES.isValid = validate(e.target.value, validators);
+            INPUT_STATES_SET({ ...INPUT_STATES });
+          }
+        }}
+        type={type}
+        id={id}
+      />
+
+      <p> {!INPUT_STATES.isValid && errorText}</p>
+    </div>
+  );
+};
+
+export default Input;
